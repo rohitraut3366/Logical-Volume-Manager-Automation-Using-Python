76535d08dbcd4f8811981d0a10dc31812dbd4b98
rohitraut3366 <59472818+rohitraut3366@users.noreply.github.com>
2020-12-17 17:42:15 +0530

    Update LVM.py

--- LVM.py
+++ LVM.py
@@ -22,7 +22,7 @@ def authTypePass(username, password, ip):
                 ---------------------------------------------------
                 -        Enter 8 : Extend VG/LV                   -
                 ---------------------------------------------------
-                -        Enter 9 : Reduce LV                      -
+                -        Enter 9 : Reduce VG/LV                      -
                 ---------------------------------------------------
                 -        Enter 10 : Exit                           -
                 ---------------------------------------------------
@@ -35,14 +35,14 @@ def authTypePass(username, password, ip):
             os.system("sshpass -p {} ssh {}@{} sudo df -hT".format(password, username, ip))
         elif choice == '3':
             create = input("Enter PV/VG/LV: ")
-            if create.lower() == 'pv':
+            if create == 'PV':
                 HD_name = input("Disk Name: ")
                 os.system("sshpass -p {} ssh {}@{} sudo pvcreate {}".format(password, username, ip, HD_name))
-            elif create.lower() == 'vg':
+            elif create == 'VG':
                 HD_name = input("Enter space separated disk names: ")
                 vg_name = input("Enter volume group Name: ")
                 os.system("sshpass -p {} ssh {}@{} sudo vgcreate {} {}".format(password, username, ip, vg_name, HD_name))
-            elif create.lower() == 'lv':
+            elif create == 'LV':
                 vg_name = input("Enter volume group name: ")
                 lv_name = input("Enter logical volume name: ")
                 lv_size = input("Enter LV size: ")
@@ -51,23 +51,23 @@ def authTypePass(username, password, ip):
                 print("Wrong create opertion")
         elif choice == '4':
             display = input("Display PV/VG/LV : ")
-            if display.lower() == 'pv':
+            if display  == 'PV':
                 os.system("sshpass -p {} ssh {}@{} sudo pvdisplay".format(password, username, ip))
-            elif display.lower() == 'vg':
+            elif display == 'VG':
                 os.system("sshpass -p {} ssh {}@{} sudo vgdisplay".format(password, username, ip))
-            elif display.lower() == 'lv':
+            elif display == 'LV':
                 os.system("sshpass -p {} ssh {}@{} sudo lvdisplay".format(password, username, ip))
             else:
                 print("Wrong create opertion")
         elif choice == '5':
             delete = input("Delete PV/VG/LV : ")
-            if delete.lower() == 'pv':
+            if delete == 'PV':
                 delete_hd = input("Enter PV name: ")
                 os.system("sshpass -p {} ssh {}@{} sudo pvremove {}".format(password, username, ip, delete_hd))
-            elif delete.lower() == 'vg':
+            elif delete == 'VG':
                 delete_hd = input("Enter VG name: ")
                 os.system("sshpass -p {} ssh {}@{} sudo vgremove {}".format(password, username, ip, delete_hd))
-            elif delete.lower() == 'lv':
+            elif delete == 'LV':
                 vg_name = input("Enter VG name: ")
                 lv_name = input("Enter LG name: ")
                 os.system("sshpass -p {} ssh {}@{} sudo lvremove /dev/{}/{}".format(password, username, ip, vg_name, lv_name))
@@ -105,14 +105,22 @@ def authTypePass(username, password, ip):
             else:
                 print("Wrong create opertion")
         elif choice == '9':
-            vg_name = input("ENTER VG NAME: ")
-            lv_name = input("ENTER NAME OF LV:")
-            new_size = input("ENTER SIZE UPTO WHICH LV SHOULD BE REDUCED:")
-            if 'n' == input("lv is mounted y/n: "):
-                os.system("sshpass -p {} ssh {}@{} sudo e2fsck -f /dev/{}/{}".format(password, username, ip, vg_name, lv_name))
-                os.system("sshpass -p {} ssh {}@{} sudo lvreduce -r -L {}G /dev/{}/{}".format(password, username, ip, new_size, vg_name, lv_name))
+            reduce = input("reduce VG/LV: ")
+            if reduce == 'LV':
+                vg_name = input("ENTER VG NAME: ")
+                lv_name = input("ENTER NAME OF LV:")
+                new_size = input("ENTER SIZE UPTO WHICH LV SHOULD BE REDUCED:")
+                if 'n' == input("lv is mounted y/n: "):
+                    os.system("sshpass -p {} ssh {}@{} sudo e2fsck -f /dev/{}/{}".format(password, username, ip, vg_name, lv_name))
+                    os.system("sshpass -p {} ssh {}@{} sudo lvreduce -r -L {}G /dev/{}/{}".format(password, username, ip, new_size, vg_name, lv_name))
+                else:
+                    print("please umount lv else you might loose online work")
+            elif reduce == 'VG':
+                pv_name = input("PV name:  it will be remove: ")
+                vg_name = input("Enter VG name: ")
+                os.system("sshpass -p {} ssh {}@{} sudo vgreduce {} {}".format(password, username, ip, vg_name, pv_name))
             else:
-                print("please umount lv else you might loose online work")
+                print("Wrong choice enter VG/LV")
         elif choice == '10':
             exit()
         else:
@@ -142,7 +150,7 @@ def authTypeKey(username, keyPath, ip):
                     ---------------------------------------------------
                     -        Enter 8 : Extend VG/LV                   -
                     ---------------------------------------------------
-                    -        Enter 9 : Reduce LV                      -
+                    -        Enter 9 : Reduce VG/LV                      -
                     ---------------------------------------------------
                     -        Enter 10 : Exit                           -
                     ----------------------------------------------------
@@ -155,15 +163,15 @@ def authTypeKey(username, keyPath, ip):
             os.system("ssh -i {} {}@{} sudo df -hT".format(keyPath, username, ip))
         elif choice == '3':
             create = input("Enter PV/VG/LV: ")
-            if create.lower() == 'pv':
+            if create == 'PV':
                 HD_name = input("Disk Name: ")
                 os.system("ssh -i {} {}@{} sudo pvcreate {}".format(keyPath, username, ip, HD_name))
-            elif create.lower() == 'vg':
+            elif create == 'VG':
                 HD_name = input("Enter space separated disk names: ")
                 vg_name = input("Enter volume group Name: ")
                 os.system(
                     "ssh -i {} {}@{} sudo vgcreate {} {}".format(keyPath, username, ip, vg_name, HD_name))
-            elif create.lower() == 'lv':
+            elif create == 'LV':
                 vg_name = input("Enter volume group name: ")
                 lv_name = input("Enter logical volume name: ")
                 lv_size = input("Enter LV size: ")
@@ -174,23 +182,23 @@ def authTypeKey(username, keyPath, ip):
                 print("Wrong create opertion")
         elif choice == '4':
             display = input("Display PV/VG/LV : ")
-            if display.lower() == 'pv':
+            if display == 'PV':
                 os.system("ssh -i {} {}@{} sudo pvdisplay".format(keyPath, username, ip))
-            elif display.lower() == 'vg':
+            elif display == 'VG':
                 os.system("ssh -i {} {}@{} sudo vgdisplay".format(keyPath, username, ip))
-            elif display.lower() == 'lv':
+            elif display == 'LV':
                 os.system("ssh -i {} {}@{} sudo lvdisplay".format(keyPath, username, ip))
             else:
                 print("Wrong create opertion")
         elif choice == '5':
             delete = input("Delete PV/VG/LV : ")
-            if delete.lower() == 'pv':
+            if delete == 'PV':
                 delete_hd = input("Enter PV name: ")
                 os.system("ssh -i {} {}@{} sudo pvremove {}".format(keyPath, username, ip, delete_hd))
-            elif delete.lower() == 'vg':
+            elif delete == 'VG':
                 delete_hd = input("Enter VG name: ")
                 os.system("ssh -i {} {}@{} sudo vgremove {}".format(keyPath, username, ip, delete_hd))
-            elif delete.lower() == 'lv':
+            elif delete == 'LV':
                 vg_name = input("Enter VG name: ")
                 lv_name = input("Enter LG name: ")
                 os.system(
@@ -223,14 +231,22 @@ def authTypeKey(username, keyPath, ip):
             else:
                 print("Wrong create opertion")
         elif choice == '9':
-            vg_name = input("ENTER VG NAME: ")
-            lv_name = input("ENTER NAME OF LV:")
-            new_size = input("ENTER SIZE UPTO WHICH LV SHOULD BE REDUCED:")
-            if 'n' == input("lv is mounted y/n: "):
-                os.system("ssh -i{} {}@{} sudo e2fsck -f /dev/{}/{}".format(keyPath, username, ip, vg_name, lv_name))
-                os.system("ssh -i{} {}@{} sudo lvreduce -r -L {}G /dev/{}/{}".format(keyPath, username, ip, new_size, vg_name, lv_name))
+            reduce = input("reduce LV/VG: ")
+            if reduce == 'LV':
+                vg_name = input("ENTER VG NAME: ")
+                lv_name = input("ENTER NAME OF LV:")
+                new_size = input("ENTER SIZE UPTO WHICH LV SHOULD BE REDUCED:")
+                if 'n' == input("lv is mounted y/n: "):
+                    os.system("ssh -i{} {}@{} sudo e2fsck -f /dev/{}/{}".format(keyPath, username, ip, vg_name, lv_name))
+                    os.system("ssh -i{} {}@{} sudo lvreduce -r -L {}G /dev/{}/{}".format(keyPath, username, ip, new_size, vg_name, lv_name))
+                else:
+                    print("please umount lv else you might loose online work")
+            elif reduce == 'VG':
+                vg_name = input("Enter VG name: ")
+                pv_name = input("pv name: ")
+                os.system("ssh -i {} {}@{} sudo vgreduce {} {}".foramt(keypath, username, ip, vg_name, pv_name))
             else:
-                print("please umount lv else you might loose online work")
+                print("Enter LV/VG")
         elif choice == '10':
             exit()
         else:
